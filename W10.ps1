c129650c24a28dcd5d60b288803fb52524e3fec8
MitchelPB <96064193+MitchelPB@users.noreply.github.com>
2024-11-16 06:06:46 -0300

    Delete W10.ps1

--- W10.ps1
+++
@@ -1,4 +0,0 @@
-powershell.exe -NoProfile -Command "irm asheroto.com/winget | iex"
-cmd.exe /c "powershell.exe -NoProfile -Command "irm https://github.com/MitchelPB/Apps/releases/download/app/Apps24-10.ps1 | iex""
-exit
-
