2da979601288777bacbd29310b93c0b7e6083af4
MitchelPB <96064193+MitchelPB@users.noreply.github.com>
2024-11-16 04:24:37 -0300

    Add files via upload

--- W10.ps1
+++ W10.ps1
@@ -1,8 +1,4 @@
-#powershell.exe -NoProfile -Command "irm asheroto.com/winget | iex"
-#cmd.exe /c "powershell.exe -NoProfile -Command "irm https://github.com/MitchelPB/Apps/releases/download/app/Apps24-10.ps1 | iex""
-#exit
+powershell.exe -NoProfile -Command "irm asheroto.com/winget | iex"
+cmd.exe /c "powershell.exe -NoProfile -Command "irm https://github.com/MitchelPB/Apps/releases/download/app/Apps24-10.ps1 | iex""
+exit
 
-$PowerFile = "https://github.com/PowerShell/PowerShell/releases/download/v7.4.6/PowerShell-7.4.6-win-x64.msi"
-Invoke-WebRequest -Uri $PowerFile -OutFile "C:\Setup\PowerShell-7.msi"
-msiexec /package C:\Setup\PowerShell-7.msi /passive
-pwsh.exe -NoProfile -Command "irm https://github.com/MitchelPB/Apps/releases/download/app/W10.ps1 | iex"
