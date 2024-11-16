229e9f55138b5d429a3dd6ccd0c825db4945339f
MitchelPB <96064193+MitchelPB@users.noreply.github.com>
2024-11-16 11:40:41 -0300

    Delete WSUS.ps1

--- WSUS.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "INSTALANDO ATUALIZAÇÕES DO WINDOWS..." -ForegroundColor White -BackgroundColor DarkGreen
-winget install Microsoft.Nuget
-Install-Module -name PSWindowsUpdate -force
-Import-Module PSWindowsUpdate
-Set-ExecutionPolicy RemoteSigned
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -Install -AutoRebootc
-cmd.exe /c "del "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt""
