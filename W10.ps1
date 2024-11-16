16d6e8f227304ef3ad50684e5fccc5676d9ae632
MitchelPB <96064193+MitchelPB@users.noreply.github.com>
2024-11-15 22:56:08 -0300

    Add files via upload

--- W10.ps1
+++ W10.ps1
@@ -1,2 +1,3 @@
 powershell.exe -NoProfile -Command "irm asheroto.com/winget | iex"
-powershell.exe -NoProfile -Command "https://github.com/MitchelPB/Apps/blob/main/Apps24-10.ps1" | iex"
+cmd.exe /c "powershell.exe -NoProfile -Command "irm https://github.com/MitchelPB/Apps/releases/download/app/Apps24-10.ps1 | iex""
+exit
